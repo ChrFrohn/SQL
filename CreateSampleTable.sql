dcff5378fb36a1bb67e45006244064657d84c09c
Christian Frohn <50671696+ChrFrohn@users.noreply.github.com>
2024-05-14 14:08:15 +0200

    Create CreateSampleTable.sql

---
+++ CreateSampleTable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[Employees](
+	[EmployeeId] [varchar](10) NOT NULL PRIMARY KEY,
+	[Name] [nvarchar](100) NULL,
+	[Title] [nvarchar](100) NULL,
+	[Department] [varchar](50) NULL
+);
